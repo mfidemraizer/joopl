458c13b940a4e743ab32ecc8965c6ada5f999ae9
Matías Fidemraizer <fidemraizer@hotmail.com>
2013-05-19 19:06:05 +0200

    Working on 2.3.0's documentation
    I've added new documentation for enumerations and, in addition, I've
    changed some documentation site styles in order to achieve a better
    look&feel!

---
+++ doc/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "$def",
+        "$enumdef",
+        "$namespace",
+        "Enum",
+        "EnumValue"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
