42a48bbf7b7f91d8a7b2256aa473e4c09f96600b
Matias Fidemraizer <fidemraizer@hotmail.com>
2013-05-21 18:27:38 +0200

    I forgot to change the 2.3.0 CDNJS link in the download button.........!!
    Also, the side-bar menu had some look&feel problems.

---
+++ doc/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "$def",
+        "$enumdef",
+        "$namespace",
+        "Attribute",
+        "Enum",
+        "EnumValue",
+        "Type"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
