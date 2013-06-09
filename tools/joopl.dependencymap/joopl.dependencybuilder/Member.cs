e1072fae2447f16554a09deea7105718d1ebc82d
Matías Fidemraizer <fidemraizer@hotmail.com>
2013-06-09 22:05:06 +0200

    Working on the Dependency Map tool
    The goal is analyzing a jOOPL-based library and get a dependency map:
    
    a) What namespaces are in the library across its multiple files?
    b) What classes and enumerations are defined in the whole namespaces?
    c) And where are the whole classes and enumerations required?
    
    The a) and b) are already answered (i.e. developed), and now's time for
    c).
    
    Having a, b and c, jOOPL will be able of determining what JavaScript
    files should be dynamically loaded into the Web browser based on what
    types are going to be used inside a $namespace.using block!

---
+++ tools/joopl.dependencymap/joopl.dependencybuilder/Member.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.Serialization;
+
+namespace joopl.dependencybuilder
+{
+    [DataContract(IsReference = false)]
+    public sealed class Member
+    {
+        [DataMember(Name = "kind")]
+        public string Kind
+        {
+            get { return "member"; }
+        }
+
+        public Namespace Parent
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "namespace")]
+        public string Namespace
+        {
+            get { return Parent.Name; }
+        }
+
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "file")]
+        public string File
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}", Parent.Name, Name);
+        }
+    }
+}
---
+++ tools/joopl.dependencymap/joopl.dependencybuilder/Namespace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace joopl.dependencybuilder
+{
+    [DataContract(IsReference = false)]
+    public sealed class Namespace
+    {
+        public Namespace()
+        {
+            Files = new List<string>();
+            Members = new List<Member>();
+        }
+
+        [DataMember(Name = "kind")]
+        public string Kind
+        {
+            get { return "namespace"; }
+        }
+
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "files")]
+        public List<string> Files
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "members")]
+        public List<Member> Members
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
