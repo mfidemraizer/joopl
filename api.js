7bd91aea9dcbb1b97e342264f953c0ae99900fe8
Matías Fidemraizer <fidemraizer@hotmail.com>
2013-11-01 23:37:22 +0100

    Organizing $namespace documentation better

--- api.js
+++ api.js
@@ -1,17 +1,17 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "$namespace",
         "Declaring enumerations with jOOPL",
         "Define classes with jOOPL",
         "Exception throwing best practices",
-        "Namespace",
-        "joopl.$namespace",
         "joopl.ArgumentException",
         "joopl.Attribute",
         "joopl.Enum",
         "joopl.EnumValue",
         "joopl.Environment",
         "joopl.Exception",
+        "joopl.Namespace",
         "joopl.NotImplementedException",
         "joopl.Object",
         "joopl.Type"
