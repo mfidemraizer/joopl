293ef7fbf1b715eb016899f079bef12f388757b4
Matías Fidemraizer <fidemraizer@hotmail.com>
2013-12-11 23:25:36 +0100

    --nologo analyzer param
    This will disable logo text and license link.

--- test/test-analyzer-hierarchy/moduleinfo.js
+++ test/test-analyzer-hierarchy/moduleinfo.js
@@ -14,14 +14,14 @@ $import.mapMany(
 	"threeclasstest.C": [
 		"./ThreeClasses.js"
 	],
+	"joopl.analyzer.ns3.C": ["./ns2/A.js",
+		"./ns2/B.js", "./ns3/C.js",
+		"./ns1/A.js", "./ns1/B.js"
+	],
 	"joopl.analyzer.ns2.A": ["./ns2/A.js"],
 	"joopl.analyzer.ns2.B": ["./ns2/A.js",
 		"./ns2/B.js", "./ns1/A.js",
 		"./ns1/B.js"
-	],
-	"joopl.analyzer.ns3.C": ["./ns2/A.js",
-		"./ns2/B.js", "./ns3/C.js",
-		"./ns1/A.js", "./ns1/B.js"
 	],
 	"joopl.analyzer.ns1.A": ["./ns1/A.js"],
 	"joopl.analyzer.ns1.B": ["./ns1/A.js",
